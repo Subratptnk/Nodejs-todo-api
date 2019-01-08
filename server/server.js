ff9fc18db7a65a7997a3bfa80d5d9b4456a904c2
Subratptnk <subratpattanaik20@gmail.com>
2019-01-08 17:57:54 +0530

    Updated GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,40 +1,55 @@
-// server of our server
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-  var express = require('express');
-  var bodyParser = require('body-parser');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
+var app = express();
 
- var{mongoose} = require('./db/mongoose.js');
- var{Todo} = require('./models/Todo.js');
- var {User} = require('./models/user');
+app.use(bodyParser.json());
 
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-
- var app = express();
- app.use(bodyParser.json());
-
- app.post('/todos',(req,res) => {
-    var todo = new Todo( {
-      text: req.body.text
-    });
-    todo.save().then((doc) => {
-      res.send(doc);
-    },(e) => {
-      res.status(400).send(e);
-        });
- });
-
-
-app.get('/todos',(req,res)=> {
-  Todo.find().then((todos) =>{
-    res.send({todos});
-  },(e) => {
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
- app.listen(3000, () => {
-   console.log("Started on port 3000");
- });
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
 
 module.exports = {app};
