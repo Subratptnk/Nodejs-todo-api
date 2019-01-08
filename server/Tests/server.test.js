95be9c9c3bf255f201f5ff7347f9cbec48f07354
Subratptnk <subratpattanaik20@gmail.com>
2019-01-08 13:38:49 +0530

    Updated POST/todos

--- server/Tests/server.test.js
+++ server/Tests/server.test.js
@@ -2,7 +2,7 @@
  const request = require('supertest');
 
  const {app} =require('./../server');
- const {Todo} = require('./../models/todo.js');
+ const {Todo} = require('./../models/Todo.js');
 
 
  beforeEach((done) => {
